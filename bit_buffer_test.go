6331311ba6ff61bc3d92104860775940aa6989a6
piglig <zhu1197437384@gmail.com>
2023-07-18 11:30:11 +0800

    chore: test cases 1. TestBitBuffer 2. TestReedSolomonMultiply 3. TestNumCharCountBits

--- bit_buffer_test.go
+++ bit_buffer_test.go
@@ -137,3 +137,55 @@ func checkEqual(b *BitBuffer, expected []bool) bool {
 	}
 	return true
 }
+
+func TestBitBuffer(t *testing.T) {
+	buffer := BitBuffer{}
+
+	// Testing len()
+	if buffer.len() != 0 {
+		t.Errorf("Expected length to be 0 but got %d", buffer.len())
+	}
+
+	// Testing set and getBit
+	buffer.set(2, true)
+	if !buffer.getBit(2) {
+		t.Errorf("Expected bit at position 2 to be true but got false")
+	}
+	if buffer.getBit(3) {
+		t.Errorf("Expected bit at position 3 to be false but got true")
+	}
+
+	// Testing grow
+	buffer.grow(10)
+	if buffer.len() != 10 {
+		t.Errorf("Expected length to be 10 but got %d", buffer.len())
+	}
+
+	// Testing appendBits
+	err := buffer.appendBits(15, 4)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+	}
+	if !buffer.getBit(10) || !buffer.getBit(11) || !buffer.getBit(12) || !buffer.getBit(13) {
+		t.Errorf("Expected bits 10-13 to be true but got false")
+	}
+
+	// Testing clone
+	cloneBuffer := buffer.clone()
+	for i := 0; i < buffer.len(); i++ {
+		if cloneBuffer.getBit(i) != buffer.getBit(i) {
+			t.Errorf("Expected bit at position %d to be equal in both buffers", i)
+		}
+	}
+
+	// Testing appendData
+	otherBuffer := BitBuffer{}
+	otherBuffer.set(0, true)
+	err = buffer.appendData(&otherBuffer)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+	}
+	if !buffer.getBit(buffer.len() - 1) {
+		t.Errorf("Expected last bit to be true but got false")
+	}
+}
