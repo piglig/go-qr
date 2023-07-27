49771e6784a718aa68194d3a4e4d3ff026603e12
piglig <zhu1197437384@gmail.com>
2023-07-27 13:52:39 +0800

    refactor: refactor the test case for appendBits (#54)
    * chore: test case for MakeSegmentsOptimally
    
    * refactor: refactor the test case for appendBits

--- bit_buffer_test.go
+++ bit_buffer_test.go
@@ -1,7 +1,7 @@
 package go_qr
 
 import (
-	"reflect"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -9,6 +9,7 @@ var M = 100000
 
 func TestEquivalence(t *testing.T) {
 	s := BitBuffer{}
+
 	for j := 2; j < M; j += 13 {
 		s.set(j, true)
 		if !s.getBit(j) {
@@ -35,157 +36,89 @@ func TestClone(t *testing.T) {
 	}
 
 	b := a.clone()
-	if !reflect.DeepEqual(a, b) {
-		t.Errorf("Expected BitBuffer a and b should be the same")
-	}
+	assert.Equal(t, a, b)
 }
 
 func TestAppendBits(t *testing.T) {
-	b := &BitBuffer{}
 
-	expected := []bool{true, false, true}
-	err := b.appendBits(5, 3)
-	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
+	tests := []struct {
+		name     string
+		original BitBuffer
+		val      int
+		length   int
+		wantErr  bool
+		wantData BitBuffer
+	}{
+		{
+			name:     "test with positive value",
+			original: BitBuffer{},
+			val:      5,
+			length:   3,
+			wantErr:  false,
+			wantData: BitBuffer{true, false, true},
+		},
+		{
+			name:     "test with negative value",
+			original: BitBuffer{},
+			val:      -100,
+			length:   5,
+			wantErr:  true,
+			wantData: BitBuffer{},
+		},
 	}
 
-	if !checkEqual(b, expected) {
-		t.Errorf("Unexpected BitBuffer state after appendBits: %v, expected: %v", *b, expected)
-	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.original.appendBits(tt.val, tt.length)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("appendBits() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 
-	err = b.appendBits(2, 2)
-	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
-	}
-
-	expected = []bool{true, false, true, true, false}
-	if !checkEqual(b, expected) {
-		t.Errorf("Unexpected BitBuffer state after appendBits: %v, expected: %v", *b, expected)
-	}
-
-	err = b.appendBits(1000, 32)
-	if err == nil {
-		t.Errorf("Expected error: %v", err)
-	}
-
-	expected = []bool{true, false, true, true, false}
-	if !checkEqual(b, expected) {
-		t.Errorf("Unexpected BitBuffer state after appendBits: %v, expected: %v", *b, expected)
+			assert.Equal(t, tt.wantData, tt.original)
+		})
 	}
 }
 
 func TestAppendData(t *testing.T) {
-	a := &BitBuffer{}
-	err := a.appendData(nil)
-	if err == nil {
-		t.Error("Expected error")
-	}
-
 	cases := []struct {
-		ABufferSet []bool
-		BBufferSet []bool
-		Expected   []bool
+		ABufferSet *BitBuffer
+		BBufferSet *BitBuffer
+		wantErr    bool
+		wantData   *BitBuffer
 	}{
 		{
-			ABufferSet: []bool{},
-			BBufferSet: []bool{true, true, false, true, false, true},
-			Expected:   []bool{true, true, false, true, false, true},
+			ABufferSet: &BitBuffer{},
+			BBufferSet: nil,
+			wantErr:    true,
+			wantData:   &BitBuffer{},
 		},
 		{
-			ABufferSet: []bool{true, true, false, true, false, false},
-			BBufferSet: []bool{},
-			Expected:   []bool{true, true, false, true, false, false},
+			ABufferSet: &BitBuffer{},
+			BBufferSet: &BitBuffer{true, true, false, true, false, true},
+			wantErr:    false,
+			wantData:   &BitBuffer{true, true, false, true, false, true},
 		},
 		{
-			ABufferSet: []bool{false, false, true, true, false, true, false, true, true, false, true},
-			BBufferSet: []bool{true, true, false, true, false, false},
-			Expected:   []bool{false, false, true, true, false, true, false, true, true, false, true, true, true, false, true, false, false},
+			ABufferSet: &BitBuffer{true, true, false, true, false, false},
+			BBufferSet: &BitBuffer{},
+			wantErr:    false,
+			wantData:   &BitBuffer{true, true, false, true, false, false},
+		},
+		{
+			ABufferSet: &BitBuffer{false, false, true, true, false, true, false, true, true, false, true},
+			BBufferSet: &BitBuffer{true, true, false, true, false, false},
+			wantErr:    false,
+			wantData:   &BitBuffer{false, false, true, true, false, true, false, true, true, false, true, true, true, false, true, false, false},
 		},
 	}
 
-	for _, c := range cases {
-		a := &BitBuffer{}
-		b := &BitBuffer{}
-		for i, v := range c.ABufferSet {
-			a.set(i, v)
+	for _, tt := range cases {
+		err := tt.ABufferSet.appendData(tt.BBufferSet)
+		if (err != nil) != tt.wantErr {
+			t.Errorf("appendData() error = %v, wantErr %v", err, tt.wantErr)
+			return
 		}
-
-		for j, v := range c.BBufferSet {
-			b.set(j, v)
-		}
-
-		err = a.appendData(b)
-		if err != nil {
-			t.Errorf("Unexpected error: %v", err)
-		}
-
-		if !checkEqual(a, c.Expected) {
-			t.Errorf("Unexpected BitBuffer state after appendData: %v, expected: %v", *a, c.Expected)
-		}
-	}
-}
-
-func checkEqual(b *BitBuffer, expected []bool) bool {
-	if b.len() != len(expected) {
-		return false
-	}
-
-	for i := range expected {
-		if b.getBit(i) != expected[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func TestBitBuffer(t *testing.T) {
-	buffer := BitBuffer{}
-
-	// Testing len()
-	if buffer.len() != 0 {
-		t.Errorf("Expected length to be 0 but got %d", buffer.len())
-	}
-
-	// Testing set and getBit
-	buffer.set(2, true)
-	if !buffer.getBit(2) {
-		t.Errorf("Expected bit at position 2 to be true but got false")
-	}
-	if buffer.getBit(3) {
-		t.Errorf("Expected bit at position 3 to be false but got true")
-	}
-
-	// Testing grow
-	buffer.grow(10)
-	if buffer.len() != 10 {
-		t.Errorf("Expected length to be 10 but got %d", buffer.len())
-	}
-
-	// Testing appendBits
-	err := buffer.appendBits(15, 4)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err.Error())
-	}
-	if !buffer.getBit(10) || !buffer.getBit(11) || !buffer.getBit(12) || !buffer.getBit(13) {
-		t.Errorf("Expected bits 10-13 to be true but got false")
-	}
-
-	// Testing clone
-	cloneBuffer := buffer.clone()
-	for i := 0; i < buffer.len(); i++ {
-		if cloneBuffer.getBit(i) != buffer.getBit(i) {
-			t.Errorf("Expected bit at position %d to be equal in both buffers", i)
-		}
-	}
-
-	// Testing appendData
-	otherBuffer := BitBuffer{}
-	otherBuffer.set(0, true)
-	err = buffer.appendData(&otherBuffer)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err.Error())
-	}
-	if !buffer.getBit(buffer.len() - 1) {
-		t.Errorf("Expected last bit to be true but got false")
+		assert.Equal(t, tt.wantData, tt.ABufferSet)
 	}
 }
--- qr_segment_advanced_test.go
+++ qr_segment_advanced_test.go
@@ -1,40 +1,52 @@
 package go_qr
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func TestIsKanji(t *testing.T) {
-	testCases := []struct {
-		name string
-		c    int
-		want bool
+func TestMakeSegmentsOptimally(t *testing.T) {
+	tests := []struct {
+		name                   string
+		text                   string
+		ecl                    Ecc
+		minVersion, maxVersion int
+		wantErr                bool
+		wantSegments           []*QrSegment
 	}{
 		{
-			name: "Test Case 1: When c is less than length of unicdeToQRKanji and unicdeToQRKanji[c] is not equal to -1",
-			c:    0, // Assuming the first index of unicdeToQRKanji is not -1 after initialization
-			want: false,
-		},
-		{
-			name: "Test Case 2: When c is more than length of unicdeToQRKanji",
-			c:    1<<16 + 5, // A number certainly greater than length of unicdeToQRKanji
-			want: false,
-		},
-		{
-			name: "Test Case 3: When c is within range but unicdeToQRKanji[c] is equal to -1",
-			c:    500, // This needs to be adjusted based on the actual content of unicdeToQRKanji
-			want: false,
-		},
-		{
-			name: "Test Case 4: When c is within range",
-			c:    65311,
-			want: true,
+			name:       "test with byte text",
+			text:       "Hello, World!",
+			ecl:        Low,
+			minVersion: 1,
+			maxVersion: 1,
+			wantErr:    false,
+			wantSegments: []*QrSegment{
+				{
+					mode:     Byte,
+					numChars: 13,
+					data: &BitBuffer{
+						false, true, false, false, true, false, false, false, false, true, true, false, false, true, false,
+						true, false, true, true, false, true, true, false, false, false, true, true, false, true, true, false,
+						false, false, true, true, false, true, true, true, true, false, false, true, false, true, true, false,
+						false, false, false, true, false, false, false, false, false, false, true, false, true, false, true,
+						true, true, false, true, true, false, true, true, true, true, false, true, true, true, false, false,
+						true, false, false, true, true, false, true, true, false, false, false, true, true, false, false, true,
+						false, false, false, false, true, false, false, false, false, true,
+					},
+				},
+			},
 		},
 	}
 
-	for _, tt := range testCases {
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := isKanji(tt.c); got != tt.want {
-				t.Errorf("isKanji() = %v, want %v", got, tt.want)
+			got, err := MakeSegmentsOptimally(tt.text, tt.ecl, tt.minVersion, tt.maxVersion)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MakeSegmentsOptimally() error = %v, wantErr %v", err, tt.wantErr)
 			}
+
+			assert.Equal(t, tt.wantSegments, got)
 		})
 	}
 }
