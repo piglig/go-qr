ae96487bad19293a7f16bc68433ef5801eca3370
piglig <zhu1197437384@gmail.com>
2023-08-01 10:58:29 +0800

    docs: add code comments

--- bit_buffer.go
+++ bit_buffer.go
@@ -5,6 +5,7 @@ import (
 	"math"
 )
 
+// BitSet defines an interface that allows manipulation of a bitset.
 type BitSet interface {
 	getBit(i int) bool
 	set(i int, value bool)
@@ -13,17 +14,20 @@ type BitSet interface {
 
 type BitBuffer []bool
 
+// len returns the length of the BitBuffer.
 func (b *BitBuffer) len() int {
 	return len(*b)
 }
 
+// set sets the bit at position i in the BitBuffer to value.
 func (b *BitBuffer) set(i int, value bool) {
 	if i >= len(*b) {
-		b.grow(1 + i)
+		b.grow(1 + i) // If index is beyond current length, grow buffer.
 	}
 	(*b)[i] = value
 }
 
+// getBit returns the bit at position i in the BitBuffer.
 func (b *BitBuffer) getBit(i int) bool {
 	if i >= len(*b) {
 		return false
@@ -31,12 +35,14 @@ func (b *BitBuffer) getBit(i int) bool {
 	return (*b)[i]
 }
 
+// grow increases the size of the BitBuffer.
 func (b *BitBuffer) grow(size int) {
 	res := make(BitBuffer, size)
 	copy(res, *b)
 	*b = res
 }
 
+// appendBits appends val as a binary number of length bits to the end of the BitBuffer.
 func (b *BitBuffer) appendBits(val, length int) error {
 	if length < 0 || length > 31 || (val>>uint(length)) != 0 {
 		return fmt.Errorf("value out of range")
@@ -50,6 +56,7 @@ func (b *BitBuffer) appendBits(val, length int) error {
 	return nil
 }
 
+// appendData appends another BitBuffer to this BitBuffer.
 func (b *BitBuffer) appendData(other *BitBuffer) error {
 	if other == nil {
 		return fmt.Errorf("BitBuffer is nil")
@@ -66,6 +73,7 @@ func (b *BitBuffer) appendData(other *BitBuffer) error {
 	return nil
 }
 
+// clone creates a copy of the BitBuffer.
 func (b *BitBuffer) clone() *BitBuffer {
 	clone := make(BitBuffer, len(*b))
 	copy(clone, *b)
