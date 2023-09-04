e370c33883f3d903b29463529013b63c8ae580a6
piglig <zhu1197437384@gmail.com>
2023-09-04 11:51:03 +0800

    refactor: test case optimization (#76)

--- qr_code_test.go
+++ qr_code_test.go
@@ -5,44 +5,6 @@ import (
 	"testing"
 )
 
-func TestGetNumRawDataModules(t *testing.T) {
-	testCases := []struct {
-		name    string
-		version int
-		want    int
-	}{
-		{
-			name:    "version equals to MinVersion",
-			version: 1,
-			want:    208,
-		},
-		{
-			name:    "version equals to MaxVersion",
-			version: 40,
-			want:    29648,
-		},
-		{
-			name:    "version between 2 and 6",
-			version: 4,
-			want:    807,
-		},
-		{
-			name:    "version equal or higher than 7",
-			version: 9,
-			want:    2336,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			got := getNumRawDataModules(tc.version)
-			if got != tc.want {
-				t.Errorf("getNumRawDataModules() = %v, want %v", got, tc.want)
-			}
-		})
-	}
-}
-
 func TestEncodeStandardSegments(t *testing.T) {
 	cases := []struct {
 		name       string
